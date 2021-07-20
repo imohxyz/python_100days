b6430d400f9f71afe8681112c5ac641bc67918a1
Imron Wignyowiyoto <masimoh98@gmail.com>
2021-07-20 16:23:07 +0700

    finishing day4

--- day4_final.py
+++ day4_final.py
@@ -1,11 +1,45 @@
 import random
 
-row1 = ["⬜️","⬜️","⬜️"]
-row2 = ["⬜️","⬜️","⬜️"]
-row3 = ["⬜️","⬜️","⬜️"]
-map1 = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-aa = list(map(int, str(position)))
-# map[position] = "x"-------------------------
-print(f"{row1}\n{row2}\n{row3}")
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print('------------Game Gunting Batu Kertas------------')
+pemain = int(input('Batu = 0, Kertas = 1, Gunting = 2 -> '))
+bot = random.randint(0, 2)
+pilihan = [rock, paper, scissors]
+nama_pilihan = ['Batu', 'Kertas', 'Gunting']
+print(f'Pemain pilih : {nama_pilihan[pemain]}\n{pilihan[pemain]}')
+print(f'Komputer pilih : {nama_pilihan[bot]}\n{pilihan[bot]}')
+
+if (pemain == 0 and bot == 2) or (pemain == 1 and bot == 0) or (pemain == 2 and bot == 1):
+    print('Pemain Menang')
+elif (bot == 0 and pemain == 2) or (bot == 1 and pemain == 0) or (bot == 2 and pemain == 1):
+    print('Komputer Menang')
+elif pemain == bot:
+    print('Seri')
+else:
+    print('Input tidak ditemukan di list')
